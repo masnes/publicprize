6d907d28b43107920d59bf74b62ebf59bc422a6a
Paul Moeller <moeller@bivio.biz>
2014-08-24 23:13:16 +0000

    added placeholders for contest pages

--- publicprize/controller.py
+++ publicprize/controller.py
@@ -11,6 +11,18 @@ def home_page():
 def contestants(biv_id):
     return flask.render_template("contest/contestants.html")
 
+@app.route('/<biv_id>/donors')
+def donors(biv_id):
+    return flask.render_template("contest/donors.html")
+
+@app.route('/<biv_id>/about')
+def about(biv_id):
+    return flask.render_template("contest/about.html")
+
+@app.route('/<biv_id>/how-to-enter')
+def how_to_enter(biv_id):
+    return flask.render_template("contest/how-to-enter.html")
+
 @app.errorhandler(404)
 def page_not_found(e):
     return flask.render_template('error/not-found.html'), 404
