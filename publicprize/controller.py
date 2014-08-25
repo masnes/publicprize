b0a5259a5b04833cfa268eb8d8e3152348696906
robnagler <github@q33.us>
2014-08-25 19:01:45 +0000

    reordered methods

--- publicprize/controller.py
+++ publicprize/controller.py
@@ -61,29 +61,24 @@ def _init():
 
 _init()
 
-@_app.errorhandler(404)
-def _route_404(e):
-    return _route(_ERROR_BIV_URI + '/' + 'not-found')
-
-@_app.route("/")
-def _route_root():
-    return _route('')
-
-@_app.route("/<path:path>")
-def _route(path):
-    # request_context
-    biv_id, action, path_info = _parse_path(path)
-    # assign path_info, etc. to request_context
-    biv_type = biv_id[-3:]
-    biv_obj = _load_biv_obj(biv_type, biv_id)
-    action_method = _lookup_action(biv_type, action)
-    return action_method(biv_obj)
-
 def _load_biv_obj(biv_type, biv_id):
     print(_biv_type_to_model_class)
     if not biv_type in _biv_type_to_model_class:
         raise ValueError(biv_type + ": unknown biv_type")
     return _biv_type_to_model_class[biv_type].load_biv_obj(biv_id)
+
+def _lookup_action(biv_type, name):
+    if len(name) == 0:
+        name = _DEFAULT_ACTION_NAME
+    tc = _biv_type_to_task_class[biv_type]
+    name = re.sub('\W', '_', name)
+    n = 'action_' + name
+    if not hasattr(tc, n):
+        raise ValueError(n + ': action not found')
+    m = getattr(tc, n)
+    if not inspect.isfunction(m):
+        raise ValueError(n + ': action not a method')
+    return m
 
 def _lookup_biv_id(biv_uri):
     if len(biv_uri) == 0:
@@ -108,19 +103,6 @@ def _lookup_biv_id(biv_uri):
     #TODO: wrap better
     raise ValueError(biv_uri + ': invalid syntax')
 
-def _lookup_action(biv_type, name):
-    if len(name) == 0:
-        name = _DEFAULT_ACTION_NAME
-    tc = _biv_type_to_task_class[biv_type]
-    name = re.sub('\W', '_', name)
-    n = 'action_' + name
-    if not hasattr(tc, n):
-        raise ValueError(n + ': action not found')
-    m = getattr(tc, n)
-    if not inspect.isfunction(m):
-        raise ValueError(n + ': action not a method')
-    return m
-
 def _parse_path(path):
     parts = path.split('/', 2)
     biv_uri = parts[0] if len(parts) >= 1 else _DEFAULT_BIV_URI
@@ -128,3 +110,22 @@ def _parse_path(path):
     path_info = parts[2] if len(parts) >= 3 else None
     biv_id = _lookup_biv_id(biv_uri)
     return biv_id, action, path_info
+
+@_app.route("/<path:path>")
+def _route(path):
+    # request_context
+    biv_id, action, path_info = _parse_path(path)
+    # assign path_info, etc. to request_context
+    biv_type = biv_id[-3:]
+    biv_obj = _load_biv_obj(biv_type, biv_id)
+    action_method = _lookup_action(biv_type, action)
+    return action_method(biv_obj)
+
+@_app.errorhandler(404)
+def _route_404(e):
+    return _route(_ERROR_BIV_URI + '/' + 'not-found')
+
+@_app.route("/")
+def _route_root():
+    return _route('')
+
