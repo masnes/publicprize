3c5c84bd4e2433c6f19efd2ac9a034fb06f7199f
Paul Moeller <moeller@bivio.biz>
2014-08-28 18:29:05 +0000

    cleaned up facebook login a bit

--- publicprize/general/task.py
+++ publicprize/general/task.py
@@ -3,14 +3,13 @@
 import flask
 from flask_oauthlib.client import OAuth, OAuthException
 from publicprize import controller
-import publicprize.auth.model as pam
-import publicprize.contest.model as pcm
+import publicprize.contest.model
 import werkzeug
 
 facebook = OAuth(controller.app()).remote_app(
     'facebook',
-    consumer_key=controller.app().config['FACEBOOK_APP_ID'],
-    consumer_secret=controller.app().config['FACEBOOK_APP_SECRET'],
+    consumer_key=controller.app().config['PP_FACEBOOK_APP_ID'],
+    consumer_secret=controller.app().config['PP_FACEBOOK_APP_SECRET'],
     request_token_params={'scope': 'email'},
     base_url='https://graph.facebook.com',
     request_token_url=None,
@@ -22,20 +21,19 @@ class General(controller.Task):
     def action_index(biv_obj):
         return flask.render_template(
             "general/index.html",
-            contests=pcm.Contest.query.all()
+            contests=publicprize.contest.model.Contest.query.all()
         )
 
     def action_facebook_login(biv_obj):
-        controller.app().logger.info(flask.request.referrer);
         callback = flask.url_for(
             '_route',
             path=biv_obj.format_uri('facebook_authorized'),
-            # TODO(pjm): store next in session
-            next=flask.request.args.get('next') or flask.request.referrer or None,
             _external=True
         )
+        # return to the "next" or referrer page when return from callback
+        controller.session()['oauth.next_uri'] = flask.request.args.get('next') or flask.request.referrer or None
         state = werkzeug.security.gen_salt(10)
-        flask.session['oauth_state'] = state
+        controller.session()['oauth.state'] = state
         return facebook.authorize(
             callback=callback,
             state=state
@@ -43,49 +41,52 @@ class General(controller.Task):
 
     def action_facebook_authorized(biv_obj):
         resp = facebook.authorized_response()
-        app = controller.app()
+        session = controller.session()
+        next = None
+        if 'oauth.next_uri' in session:
+            next = session['oauth.next_uri']
+            del session['oauth.next_uri']
+        if not General._validate_facebook_auth(resp):
+            return flask.redirect(next or '/')
+        session['oauth.token'] = (resp['access_token'], '')
+        me = facebook.get('/me', token=(resp['access_token'], ''))
+        controller.app().logger.info(me.data)
+        return flask.redirect(next or '/')
+        
+    def action_not_found(biv_obj):
+        return flask.render_template('general/not-found.html'), 404
 
+    def _validate_facebook_auth(resp):
+        app = controller.app()
+        
         if resp is None:
             # TODO(pjm): flash message "facebook access denied"
-            app.logger.info(
+            app.logger.warn(
                 'Access denied: reason=%s error=%s' % (
                     flask.request.args.get('error_reason'),
                     flask.request.args.get('error_description')
                 )
             )
-            return General.action_index(biv_obj)
+            return False
         if isinstance(resp, OAuthException):
             # TODO(pjm): flash message "facebook access denied"
-            app.logger.info('Access denied: %s' % resp.message)
-            return General.action_index(biv_obj)
-        if flask.session.get('oauth_state') != flask.request.args.get('state'):
-            app.logger.info(
+            app.logger.warn(resp)
+            return False
+        session = controller.session()
+        state = session['oauth.state']
+        del session['oauth.state']
+        if state != flask.request.args.get('state'):
+            # TODO(pjm): flash message "facebook access denied"
+            app.logger.warn(
                 'Invalid oauth state, expected: %s response: %s' % (
-                    flask.session.get('oauth_state'),
+                    state,
                     flask.request.args.get('state')
                  )
             )
-            return General.action_index(biv_obj)
-
-#        flask.session['oauth_token'] = (resp['access_token'], '')
-        app.logger.info(resp)
-        token = (resp['access_token'], '')
-        me = facebook.get(
-            '/me',
-            token=token
-        )
-        app.logger.info(me.data)
-        app.logger.info(
-            'Logged in as id=%s name=%s redirect=%s' % (
-                me.data['id'], me.data['name'], flask.request.args.get('next'))
-        )
-        app.logger.info(flask.request.args);
-        return flask.redirect(flask.request.args.get('next') or '/');
-        
-    def action_not_found(biv_obj):
-        return flask.render_template('general/not-found.html'), 404
+            return False
+        return True
 
 @facebook.tokengetter
 def _get_facebook_oauth_token():
-    return flask.session.get('oauth_token')
+    return controller.session().get('oauth.token')
     
