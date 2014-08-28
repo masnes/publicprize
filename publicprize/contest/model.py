297e16574b9918652e59fc634afa7ba53b25f831
Paul Moeller <moeller@bivio.biz>
2014-08-28 02:26:37 +0000

    changed biv_id to Numeric(18) added primary key Sequences

--- publicprize/contest/model.py
+++ publicprize/contest/model.py
@@ -4,15 +4,17 @@ from publicprize.controller import db
 from publicprize import controller
 from publicprize import biv
 
-# TODO(pjm): change biv_ids to Numeric(18) with sequence
-
 class Contest(db.Model, controller.Model):
-    biv_id = db.Column(db.Integer, primary_key=True)
+    biv_id = db.Column(
+        db.Numeric(18),
+        db.Sequence('contest_s', start=1002, increment=1000),
+        primary_key=True
+    )
     display_name = db.Column(db.String(100), nullable=False)
     tag_line = db.Column(db.String(500))
     # TODO(pjm): move logo and founder_avatar to separate model BivImage
     contest_logo = db.Column(db.LargeBinary)
-    logo_type = db.Column(db.Enum('gif', 'png', 'jpeg'))
+    logo_type = db.Column(db.Enum('gif', 'png', 'jpeg', name='logo_type'))
 
     def contestant_count(self):
         # TODO(pjm): count related Contestant records
@@ -23,19 +25,27 @@ class Contest(db.Model, controller.Model):
         return 802
 
 class Contestant(db.Model, controller.Model):
-    biv_id = db.Column(db.Integer, primary_key=True)
+    biv_id = db.Column(
+        db.Numeric(18),
+        db.Sequence('contestant_s', start=1003, increment=1000),
+        primary_key=True
+    )
     display_name = db.Column(db.String(100), nullable=False)
     youtube_code = db.Column(db.String(500))
     slideshow_code = db.Column(db.String(500))
     contestant_desc = db.Column(db.String)
-    tax_id = db.Column(db.Integer)
+    tax_id = db.Column(db.Numeric(9))
 
 class Founder(db.Model, controller.Model):
-    biv_id = db.Column(db.Integer, primary_key=True)
+    biv_id = db.Column(
+        db.Numeric(18),
+        db.Sequence('founder_s', start=1004, increment=1000),
+        primary_key=True
+    )
     display_name = db.Column(db.String(100), nullable=False)
     founder_desc = db.Column(db.String)
     founder_avatar = db.Column(db.LargeBinary)
-    avatar_type = db.Column(db.Enum('gif', 'png', 'jpeg'))
+    avatar_type = db.Column(db.Enum('gif', 'png', 'jpeg', name='avatar_type'))
 
 Contest.BIV_MARKER = biv.register_marker(2, Contest)
 Contestant.BIV_MARKER = biv.register_marker(3, Contestant)
