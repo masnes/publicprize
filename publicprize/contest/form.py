6b805e160e12929582520289dbac59cefaecc33e
Paul Moeller <moeller@bivio.biz>
2014-09-01 15:53:23 +0000

    moved form execution from task to form

--- publicprize/contest/form.py
+++ publicprize/contest/form.py
@@ -1,6 +1,10 @@
 # Copyright (c) 2014 bivio Software, Inc.  All rights reserved.
 
+import flask
 from flask.ext.wtf import Form
+import publicprize.auth.model as pam
+import publicprize.contest.model as pcm
+from publicprize import controller
 from wtforms import StringField, TextAreaField
 from wtforms.validators import DataRequired
 
@@ -17,3 +21,42 @@ class ContestantForm(Form):
     )
     founder_desc = StringField('Founder Short Bio', validators=[DataRequired()])
     website = StringField('Project Website')
+
+    def execute(self, contest):
+        if self.is_submitted() and self.validate():
+            c = self._update_models(contest)
+            if c:
+                return flask.redirect(c.format_uri('contestant'))
+        return flask.render_template(
+            'contest/submit.html',
+            contest=contest,
+            form=self
+        )
+        
+    def validate_and_execute(self, contest):
+        if self.validate_on_submit():
+            return form
+        return False
+
+    def _update_models(self, contest):
+        c = pcm.Contestant()
+        self.populate_obj(c)
+        f = pcm.Founder()
+        self.populate_obj(f)
+        f.display_name = flask.session['user.display_name']
+        controller.db.session.add(c)
+        controller.db.session.add(f)
+        controller.db.session.flush()
+        controller.db.session.add(
+            pam.BivAccess(
+                source_biv_id=contest.biv_id,
+                target_biv_id=c.biv_id
+            )
+        )
+        controller.db.session.add(
+            pam.BivAccess(
+                source_biv_id=c.biv_id,
+                target_biv_id=f.biv_id
+            )
+        )
+        return c
