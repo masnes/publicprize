65bbd9698dea480ef941e4641f14f8a8c1cb54ef
Paul Moeller <moeller@bivio.biz>
2014-09-01 15:53:33 +0000

    moved form execute from task to form

--- publicprize/contest/task.py
+++ publicprize/contest/task.py
@@ -5,7 +5,6 @@ import publicprize.contest.form as pcf
 import publicprize.contest.model as pcm
 import publicprize.controller as ppc
 import publicprize.auth.model as pam
-from publicprize import controller
 
 class Contest(ppc.Task):
     def action_about(biv_obj):
@@ -31,30 +30,7 @@ class Contest(ppc.Task):
             'image/' + biv_obj.logo_type
         )
     def action_submit_contestant(biv_obj):
-        form = pcf.ContestantForm()
-        if form.validate_on_submit():
-            c = pcm.Contestant()
-            form.populate_obj(c)
-            f = pcm.Founder()
-            form.populate_obj(f)
-            f.display_name = flask.session['user.display_name']
-            controller.db.session.add(c)
-            controller.db.session.add(f)
-            controller.db.session.flush()
-            controller.db.session.add(
-                pam.BivAccess(
-                    source_biv_id=biv_obj.biv_id,
-                    target_biv_id=c.biv_id
-                )
-            )
-            controller.db.session.add(
-                pam.BivAccess(
-                    source_biv_id=c.biv_id,
-                    target_biv_id=f.biv_id
-                )
-            )
-            return flask.redirect(c.format_uri('contestant'))
-        return Contest._render_template(biv_obj, 'submit', form=form)
+        return pcf.ContestantForm().execute(biv_obj)
     def _render_template(biv_obj, name, **kwargs):
         return flask.render_template(
             "contest/" + name + ".html",
