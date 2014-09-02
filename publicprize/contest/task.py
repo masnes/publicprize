bca53241c7ce56501e3ced30802190ac200662e5
Paul Moeller <moeller@bivio.biz>
2014-09-02 18:13:20 +0000

    added docstrings

--- publicprize/contest/task.py
+++ publicprize/contest/task.py
@@ -1,4 +1,10 @@
-# Copyright (c) 2014 bivio Software, Inc.  All rights reserved.
+# -*- coding: utf-8 -*-
+""" controller actions for Contest, Contestand and Founder
+
+    :copyright: Copyright (c) 2014 Bivio Software, Inc.  All Rights Reserved.
+    :license: Apache, see LICENSE for more details.
+"""
+
 import flask
 import io
 import publicprize.contest.form as pcf
@@ -7,9 +13,12 @@ import publicprize.controller as ppc
 import publicprize.auth.model as pam
 
 class Contest(ppc.Task):
+    """Contest actions"""
     def action_about(biv_obj):
+        """About page"""
         return Contest._render_template(biv_obj, 'about')
     def action_contestants(biv_obj):
+        """Public contestant list"""
         return Contest._render_template(
             biv_obj,
             'contestants',
@@ -19,19 +28,25 @@ class Contest(ppc.Task):
             ).filter(pcm.Contestant.is_public == True).all()
         )
     def action_donors(biv_obj):
+        """Donors page"""
         return Contest._render_template(biv_obj, 'donors')
     def action_how_to_enter(biv_obj):
+        """How-to-Enter page"""
         return Contest._render_template(biv_obj, 'how-to-enter')
     def action_index(biv_obj):
+        """Default to contestant list"""
         return Contest.action_contestants(biv_obj)
     def action_logo(biv_obj):
+        """Contestant logo image"""
         return flask.send_file(
             io.BytesIO(biv_obj.contest_logo),
             'image/' + biv_obj.logo_type
         )
     def action_submit_contestant(biv_obj):
+        """Submit project page"""
         return pcf.ContestantForm().execute(biv_obj)
     def _render_template(biv_obj, name, **kwargs):
+        """Render the page, putting the selected menu and contest in env"""
         return flask.render_template(
             "contest/" + name + ".html",
             contest=biv_obj,
@@ -40,7 +55,9 @@ class Contest(ppc.Task):
         )
 
 class Contestant(ppc.Task):
+    """Contestant actions"""
     def action_contestant(biv_obj):
+        """Project detail page, loads contest owner and project founders"""
         return flask.render_template(
             'contest/detail.html',
             contest=pcm.Contest.query.select_from(pam.BivAccess).filter(
@@ -54,10 +71,13 @@ class Contestant(ppc.Task):
             ).all()
         )
     def action_index(biv_obj):
+        """Default to contestant page"""
         return Contestant.action_contestant(biv_obj)
     
 class Founder(ppc.Task):
+    """Founder actions"""
     def action_founder_avatar(biv_obj):
+        """Founder avatar image"""
         return flask.send_file(
             io.BytesIO(biv_obj.founder_avatar),
             'image/' + biv_obj.avatar_type
