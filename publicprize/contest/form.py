792daf399bfd62f995e659ac42a6707a3e684800
Paul Moeller <moeller@bivio.biz>
2014-09-02 16:02:56 +0000

    added url validation

--- publicprize/contest/form.py
+++ publicprize/contest/form.py
@@ -5,21 +5,25 @@ from flask.ext.wtf import Form
 import publicprize.auth.model as pam
 import publicprize.contest.model as pcm
 from publicprize import controller
+import re
+import urllib.request
 from wtforms import StringField, TextAreaField
 from wtforms.validators import DataRequired
 
 class ContestantForm(Form):
     display_name = StringField('Project Name', validators=[DataRequired()])
-    contestant_desc = TextAreaField('Project Summary', validators=[DataRequired()])
+    contestant_desc = TextAreaField(
+        'Project Summary', validators=[DataRequired()])
     youtube_code = StringField(
-        'YouTube Video Code',
+        'YouTube Video URL',
         validators=[DataRequired()]
     )
     slideshow_code = StringField(
-        'SlideShare Pitch Deck Code',
+        'SlideShare Pitch Deck URL',
         validators=[DataRequired()]
     )
-    founder_desc = StringField('Founder Short Bio', validators=[DataRequired()])
+    founder_desc = StringField(
+        'Founder Short Bio', validators=[DataRequired()])
     website = StringField('Project Website')
 
     def execute(self, contest):
@@ -32,15 +36,31 @@ class ContestantForm(Form):
             contest=contest,
             form=self
         )
-        
-    def validate_and_execute(self, contest):
-        if self.validate_on_submit():
-            return form
+
+    def validate(self):
+        if super().validate():
+            self._validate_youtube()
+            self._validate_slideshare()
+            self._validate_website()
+            if not self.errors:
+                return True
         return False
 
+    def _slideshare_code(self):
+        # www.slideshare.net/benjaminevans/culture-kitchen-pitch-deck-18074260
+        # www.slideshare.net/slideshow/embed_code/18074260
+        value = self.slideshow_code.data
+        m = re.search('(\d{5}\d+)$', value)
+        if m:
+            return m.group(1)
+        return None
+        
     def _update_models(self, contest):
+        self.youtube_code.data = self._youtube_code()
+        self.slideshow_code.data = self._slideshare_code()
         c = pcm.Contestant()
         self.populate_obj(c)
+        c.is_public = controller.app().config['PP_ALL_PUBLIC_CONTESTANTS']
         f = pcm.Founder()
         self.populate_obj(f)
         f.display_name = flask.session['user.display_name']
@@ -60,3 +80,53 @@ class ContestantForm(Form):
             )
         )
         return c
+
+    def _youtube_code(self):
+        value = self.youtube_code.data
+        # http://youtu.be/a1Y73sPHKxw
+        # or https://www.youtube.com/watch?v=a1Y73sPHKxw
+        if re.search('\?', value) and re.search('v\=', value):
+            m = re.search(r'(?:\?|\&)v\=(.*)', value)
+            if m:
+                return m.group(1)
+        else:
+            m = re.search(r'\/([^\&\?\/]+)$', value)
+            if m:
+                return m.group(1)
+        return None
+
+    def _validate_url(self, url):
+        if not re.search(r'^http', url):
+            url = 'http://' + url
+        try:
+            req = urllib.request.urlopen(url, None, 10)
+            req.read()
+            req.close()
+        except urllib.request.URLError:
+            return False
+        except ValueError:
+            return False
+        return True
+
+    def _validate_website(self):
+        if self.website.data:
+            if not self._validate_url(self.website.data):
+                self.website.errors = ['Website invalid or unavailable']
+
+    def _validate_youtube(self):
+        code = self._youtube_code()
+        if code:
+            if not self._validate_url('http://youtu.be/' + code):
+                self.youtube_code.errors = [
+                    'Unknown YouTube VIDEO_ID: ' + code]
+        else:
+            self.youtube_code.errors = ['Invalid YouTube URL']
+
+    def _validate_slideshare(self):
+        code = self._slideshare_code()
+        if code:
+            if not self._validate_url(
+                'http://www.slideshare.net/slideshow/embed_code/' + code):
+                self.slideshow_code.errors = ['Unknown SlideShare ID: ' + code]
+        else:
+            self.slideshow_code.errors = ['Invalid SlideShare URL']
