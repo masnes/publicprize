6dbf04218c40fff747070bb0bc2facf5e5c19926
Paul Moeller <moeller@bivio.biz>
2014-09-02 16:03:31 +0000

    implemented contestant_count() and donor_count() added contestant.is_public added Donor model

--- publicprize/contest/model.py
+++ publicprize/contest/model.py
@@ -3,6 +3,8 @@
 from publicprize.controller import db
 from publicprize import controller
 from publicprize import biv
+import publicprize.auth.model as pam
+import sqlalchemy.orm
 
 class Contest(db.Model, controller.Model):
     biv_id = db.Column(
@@ -17,12 +19,23 @@ class Contest(db.Model, controller.Model):
     logo_type = db.Column(db.Enum('gif', 'png', 'jpeg', name='logo_type'))
 
     def contestant_count(self):
-        # TODO(pjm): count related Contestant records
-        return 12
+        return pam.BivAccess.query.select_from(Contestant).filter(
+            pam.BivAccess.source_biv_id == self.biv_id,
+            pam.BivAccess.target_biv_id == Contestant.biv_id,
+            Contestant.is_public == True
+        ).count()
 
     def donor_count(self):
-        # TODO(pjm): count related Donor records
-        return 802
+        access_alias = sqlalchemy.orm.aliased(pam.BivAccess)
+        return Donor.query.select_from(pam.BivAccess, access_alias).filter(
+            pam.BivAccess.source_biv_id == self.biv_id,
+            pam.BivAccess.target_biv_id == access_alias.source_biv_id,
+            access_alias.target_biv_id == Donor.biv_id
+        ).count()
+
+    def user_has_submission(self):
+        # TODO(pjm): check if current user is a founder for this contest
+        return False
 
 class Contestant(db.Model, controller.Model):
     biv_id = db.Column(
@@ -36,7 +49,15 @@ class Contestant(db.Model, controller.Model):
     contestant_desc = db.Column(db.String)
     tax_id = db.Column(db.Numeric(9))
     website = db.Column(db.String(100))
+    is_public = db.Column(db.Boolean, nullable=False)
 
+class Donor(db.Model, controller.Model):
+    biv_id = db.Column(
+        db.Numeric(18),
+        db.Sequence('donor_s', start=1007, increment=1000),
+        primary_key=True
+    )
+    
 class Founder(db.Model, controller.Model):
     biv_id = db.Column(
         db.Numeric(18),
@@ -50,4 +71,5 @@ class Founder(db.Model, controller.Model):
 
 Contest.BIV_MARKER = biv.register_marker(2, Contest)
 Contestant.BIV_MARKER = biv.register_marker(3, Contestant)
+Donor.BIV_MARKER = biv.register_marker(7, Donor)
 Founder.BIV_MARKER = biv.register_marker(4, Founder)
