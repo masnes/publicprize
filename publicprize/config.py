975eb732a5117f304530a1a9fabc23e3f93a346f
Paul Moeller <moeller@bivio.biz>
2014-09-02 16:01:37 +0000

    added PP_ALL_PUBLIC_CONTESTANTS config

--- publicprize/config.py
+++ publicprize/config.py
@@ -3,20 +3,19 @@
 import os
 
 class BaseConfig(object):
-    SECRET_KEY = "ppsecret"
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     PP_FACEBOOK_APP_ID = os.environ.get('FACEBOOK_APP_ID')
     PP_FACEBOOK_APP_SECRET = os.environ.get('FACEBOOK_APP_SECRET')
 
 class DevConfig(BaseConfig):
+    SECRET_KEY = "ppsecret"
     DEBUG = True
     PP_DATABASE = 'pp'
     PP_DATABASE_USER = 'ppuser'
     PP_DATABASE_PASSWORD = 'pppass'
     SQLALCHEMY_DATABASE_URI = 'postgresql://' + PP_DATABASE_USER + ':' + PP_DATABASE_PASSWORD + '@localhost/' + PP_DATABASE
 #    SQLALCHEMY_ECHO = True
+    PP_ALL_PUBLIC_CONTESTANTS = True
 
 class ProdConfig(BaseConfig):
-    # TODO(pjm): read from file or environ
-    SECRET_KEY = ""
-    
+    pass
