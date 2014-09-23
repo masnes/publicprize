5da3b2c7cf2b818b1d4f26d8d49e7e2e0908044d
Michael Asnes <mike.asnes@gmail.com>
2014-09-23 17:42:02 -0600

    added some test data
    currently only for the submit entry form

---
+++ tests/test_data.py
@@ -0,0 +1,210 @@
+class SubmitEntryData(object):
+    """
+        Stores good and bad values for possible entries into the Submit Entry
+        page
+    """
+    def __init__(self):
+        self.bad_display_names = ['',
+                                  'Absurdly long string of text that no one '
+                                  'would reasonably enter for a name']
+        self.good_display_names = ['Generic Name',
+                                   'Name with 123 Numbers',
+                                   'Name with @#$%^&*() special characters',
+                                   'SingleWord',
+                                  ]
+
+        self.bad_descs = ['']
+        self.good_descs = ['Generic description',
+                           '!@#$%^&*()_+=-',
+                           '1000 blaas: blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa '
+                           'blaa blaa blaa']
+
+        self.good_contestant_descriptions = self.good_descs
+        self.bad_contestant_descriptions = self.bad_descs
+
+        self.good_youtube_urls = ['https://www.youtube.com/watch?v=K5pZlBgXBu0']
+        self.bad_youtube_urls = ['',
+                                 'https://www.youtube.co/watch?v=K5pZlBgXBu0',
+                                 'https://www.youtube.com/watch?v=K5pZlBgXBu0lkjsdfjal',
+                                 'http://www.slideshare.net/Experian_US/how-to-juggle-debt-retirement',
+                                 'https://www.google.com']
+
+        self.good_slideshow_urls = ['http://www.slideshare.net/Experian_US/how-to-juggle-debt-retirement',
+                                    'https://www.slideshare.net/Experian_US/how-to-juggle-debt-retirement']
+        self.bad_slideshow_urls = ['',
+                                   'https://www.youtube.com/watch?v=K5pZlBgXBu0',
+                                   'http://www.slideshare.net/Experian_US/how-to-juggle-debt-retirement_bad_link']
+
+        self.good_website_urls = ['www.google.com',
+                                  'https://www.google.com',
+                                  # Use long url maker to go to google.com
+                                  'http://www.longurlmaker.com/go?id=UAOJNVKBMQUGPYZKCQZRZKJEXRCRXMRSMFBZBMBODWUSVTDXJCPJMYOKQQBODSGPYHPZURNXNIQIBUGLWVUJUIUKZVYCOFSNMIMHZAEMIVVKPCVMYPVTYPEYLDBAITNUCACBBHOKCKUSMPEKFADEPHCBLEBTXPOZPQBQXPBAZVSMLXXRXAVMIYNTCPNAEBAURBKORMORHHDTEAKBBGOKEBEGRRHVSHPKIAYZNNQUVUOZUYAVEOGJQKQIBXEUEUEMUDMHRDCMXQLRPMNUBUDSETAGQEAUZFHTITBZXEMUVYMLKAFMUJFYCFFTKGOJLWDUPFUMKGHGETRPTXCNGHMJNRCYXRIINLCDRWQCDXIHRZXKWZYDHKMUCPSZHAIUMLYEIOGMMPUDPROMRNPYXCTARLAYMJTYURDDGKPSZKWOBLBTYQRWSKWKWWJIFCHAULEAVUTUEQJFBKZZBQWUBSEYPNGUQOVKZZKVTEPXUZDVKCVLTRFUKKTZXZTOOPYOOJJINYGHYJDJMYUFQAAAKTZITTXHIVVXEEGSDMZCVDLICGNSGNTRHSAAMXIVSDTXIZPMLPOHTZPVFCNMQHEXZEYMCGHVKBSSAIEMYTTRTJMZMALCHQAHUZUWFBRPCKIDTNPSGIJASWZEWLHEBHMVGGSMIKBLVKOPXDHEMREENEIKPQPQXBMEHFRQQTBLDPBBTIFGZKLNOUYDKNUIPGNXMENCXOOBEPCYYHEYRQLFKJJUXDCMKQJWUWZSLNTQCVOBCTZUJKATUJOSNRFXHOUBLTTXGNNJIBKLVKGEVGYQQMIEENCLCWMNQGKXTXHCZSNVCTZXAYNRLVVPJXALUNZLUKIOIPQHHDDKXDHXBUPMQKCZIDLCQKNLVWZDLPLTTOERRLPSGEFWOIWXLIZCTNNOJNRVDCPXRTOIEEBKIHHXPDUBLUDEHRTQFKMINBFEUUNZYOKHVRELVZMHTQMBIFRNQEWDFCIAWVZVJKCFBHRXGEEAUQBDWTRMXNQCQYDMUDHENKKORBMXFQXAHYEESVQPJHSZGVMAYUFLEPAWRMTWDRXKQCPIXGXHNPHULTVJOAVTQWPHJJEMACXREMABSXJGNQAZKVJZWESMAHTJQYWRATJEGJGYHPEVGFUQADQWHIJIQDSGBOYBIHFORLNYBSTHXNYYQOVYWEHMHZTIOSKXAPMRBZPCSJKPWIONXJLTOTGWSAFHTPFTFRLGQAJIKTYGCMTZWFTLYAHRAMZUCEOHVZOMAYVKRUQUHKTDPMPONWFOJFIMJWTFVIRWGNGYHXTPHMSXZILNFRBOWJAGGULCDDYKQEJYBCNJPGGPOSCTKDIHNJNTEYVHCTSSYBRAEEJTLQJZJLTTOCBCLOVQNRXPLQIJSZKWETQPKZPTKJNZLOCXDXNQPLFABOKTNVQSOGHZGXSRGGQSVSPYQCOGNUHPISJWNZOCGWBMTUDZBURWNHUDJIKXDRNMJWIWWWZCTAOMVSMWMDSZLMDVVNTYFGKODTLZQKBJLQVGIHCVLVUWHYSDMLBRRMGUGSUWDVGOMCVUKYPVTKSAIKDVWFMNSTIYLCVOYBCKEYFOWVKPGCQPNTKJZXXRQFQBIMQHOTRSRXGNTQCZTSOHMZQLWODMUUOCGEJUXBMOZSCSJERDWGKJFBUCPYPJSDMYIWSGYFVXYXQHBIKYOUHUVCXLAMVSPIQYEJEDOZBMWRUYZEWOZEJVGGGGSCZIKQGPZLSOKTAHKUFKZCYYHIUNOBUHDUPNKWDKHVYSPZAAUGKTIJSQRMDGNYNRSDFDAINIELAULBUFHFZQPRGHEKNRIBJBFOEFWRNADOUPPPUWVTNKLTRPDFHLHQNMERSBJGCNUXCKMXHHRUSCNJRRPZDWQQJUIBWSHYFCVINIFVQWPIZDSRUHQXEGONBWPYOXWUASCNAIIRFYZFBSWWANTEUISVEITTFQNFIQTJZCAEAZKCSGYSTSWNAPJFXDYCTLICCCLBEMGFLQHEWGWQZSEAINFFQDHKPQMRSYTWKZCWQJANQWDPPHPXVUCMYKWOBJG'
+                                  ]
+        self.bad_website_urls = ['',
+                                 'lsjdfl.alksdjflkdsj.clkj',
+                                 'www.g00gle.com']
+
+        self.good_founder_descriptions = self.good_descs
+        self.bad_founder_descriptions = self.bad_descs
+
+        self.good_founder2_descriptions = self.good_descs
+        self.bad_founder2_descriptions = self.bad_descs
+
+        self.good_tax_ids = ['22-7777777']
+        self.bad_tax_ids = ['',
+                            '99999999999999999',
+                            '22-777777a',
+                            '22-77a7777',
+                            '22-77!777a',
+                            '22-777*77a',
+                            '22M777777a',
+                            '22-7}7777a',
+                            'ab-cdefghi']
+
+        self.good_phone_nums = ['303-123-4567',
+                                '303 123 4567',
+                                '1-303-123-4567',
+                                '1 303 123 4567',
+                                '303-123-4567-3576',
+                                '303 123 4567-3576',
+                                '303 123 4567 3576',
+                                '1-303-123-4567-3576',
+                                '1 303 123 4567-3576',
+                                '1 303 123 4567 3576']
+
+        self.bad_phone_nums = ['',
+                               '1256',
+                               '303 abc defg',
+                               '303-abc-defg']
+
+        self.good_addresses = ['123 Pearl St\nBoulder CO 80303']
+        self.bad_addresses = ['']
+
+        self.good_agree_to_terms = [True]
+        self.bad_agree_to_terms = [False]
+
+        def get_good_data(self):
+            self.items_to_combine = [self.good_display_names,
+                                     self.good_contestant_descriptions,
+                                     self.good_youtube_urls,
+                                     self.good_slideshow_urls,
+                                     self.good_website_urls,
+                                     self.good_founder_descriptions,
+                                     self.good_founder2_descriptions,
+                                     self.good_tax_ids,
+                                     self.good_phone_nums,
+                                     self.good_agree_to_terms]
+            return self.items_to_combine
+
+        def get_bad_data(self):
+            self.items_to_combine = [self.bad_display_names,
+                                     self.bad_contestant_descriptions,
+                                     self.bad_youtube_urls,
+                                     self.bad_slideshow_urls,
+                                     self.bad_website_urls,
+                                     self.bad_founder_descriptions,
+                                     self.bad_founder2_descriptions,
+                                     self.bad_tax_ids,
+                                     self.bad_phone_nums,
+                                     self.bad_agree_to_terms]
+            return self.items_to_combine
+
