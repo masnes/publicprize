941b9dd9e99012f5e68ae4d95887f5f2390a683e
Michael Asnes <mike.asnes@gmail.com>
2014-09-24 13:31:30 -0600

    test_data improved data formatting

--- tests/test_data.py
+++ tests/test_data.py
@@ -2,8 +2,24 @@ class SubmitEntryData(object):
     """
         Stores good and bad values for possible entries into the Submit Entry
         page
+
+        Data:
+            'display_name'          self.[good|bad]_display_names
+            'contestant_desc'       self.[good|bad]_contestant_descriptions
+            'youtube_url'           self.[good|bad]_youtube_urls
+            'slideshow_url'         self.[good|bad]_slideshow_urls
+            'website'               self.[good|bad]_website_urls
+            'founder_desc'          self.[good|bad]_founder_descriptions
+            'tax_id'                self.[good|bad]_tax_ids
+            'business_phone'        self.[good|bad]_phone_nums
+            'business_address'      self.[good|bad]_addresses
+            'founder2_name'         self.[good|bad]_founder2_names
+            'founder2_desc'         self.[good|bad]_founder2_descriptions
+            'agree_to_terms'        self.[good|bad]_agree_to_terms
     """
+
     def __init__(self):
+
         self.bad_display_names = ['',
                                   'Absurdly long string of text that no one '
                                   'would reasonably enter for a name']
@@ -146,9 +162,6 @@ class SubmitEntryData(object):
         self.good_founder_descriptions = self.good_descs
         self.bad_founder_descriptions = self.bad_descs
 
-        self.good_founder2_descriptions = self.good_descs
-        self.bad_founder2_descriptions = self.bad_descs
-
         self.good_tax_ids = ['22-7777777']
         self.bad_tax_ids = ['',
                             '99999999999999999',
@@ -179,32 +192,43 @@ class SubmitEntryData(object):
         self.good_addresses = ['123 Pearl St\nBoulder CO 80303']
         self.bad_addresses = ['']
 
+        self.good_founder2_names = self.good_display_names
+        self.bad_founder2_names = self.good_display_names
+
+        self.good_founder2_descriptions = self.good_descs
+        self.bad_founder2_descriptions = self.bad_descs
+
         self.good_agree_to_terms = [True]
         self.bad_agree_to_terms = [False]
 
-        def get_good_data(self):
-            self.items_to_combine = [self.good_display_names,
-                                     self.good_contestant_descriptions,
-                                     self.good_youtube_urls,
-                                     self.good_slideshow_urls,
-                                     self.good_website_urls,
-                                     self.good_founder_descriptions,
-                                     self.good_founder2_descriptions,
-                                     self.good_tax_ids,
-                                     self.good_phone_nums,
-                                     self.good_agree_to_terms]
-            return self.items_to_combine
+    def get_good_data(self):
+        self.items_to_combine = [self.good_display_names,
+                                 self.good_contestant_descriptions,
+                                 self.good_youtube_urls,
+                                 self.good_slideshow_urls,
+                                 self.good_website_urls,
+                                 self.good_founder_descriptions,
+                                 self.good_tax_ids,
+                                 self.good_phone_nums,
+                                 self.good_addresses,
+                                 self.good_founder2_names,
+                                 self.good_founder2_descriptions,
+                                 self.good_agree_to_terms]
+        return self.items_to_combine
 
-        def get_bad_data(self):
-            self.items_to_combine = [self.bad_display_names,
-                                     self.bad_contestant_descriptions,
-                                     self.bad_youtube_urls,
-                                     self.bad_slideshow_urls,
-                                     self.bad_website_urls,
-                                     self.bad_founder_descriptions,
-                                     self.bad_founder2_descriptions,
-                                     self.bad_tax_ids,
-                                     self.bad_phone_nums,
-                                     self.bad_agree_to_terms]
-            return self.items_to_combine
+    def get_bad_data(self):
+        self.items_to_combine = [self.bad_display_names,
+                                 self.bad_contestant_descriptions,
+                                 self.bad_youtube_urls,
+                                 self.bad_slideshow_urls,
+                                 self.bad_website_urls,
+                                 self.bad_founder_descriptions,
+                                 self.bad_tax_ids,
+                                 self.bad_phone_nums,
+                                 self.bad_addresses,
+                                 self.bad_founder2_names,
+                                 self.bad_founder2_descriptions,
+                                 self.bad_agree_to_terms]
+        return self.items_to_combine
+
 
