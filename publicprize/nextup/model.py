d107c3ccd742fd6d447eaad4fb82a7a72189255e
Paul Moeller <moeller@bivio.biz>
2015-01-21 19:07:32 +0000

    renamed Nomination to Nominator added Nominee.display_name and Nominator.display_name

--- publicprize/nextup/model.py
+++ publicprize/nextup/model.py
@@ -9,6 +9,7 @@ import flask
 
 from .. import biv
 from .. import common
+from ..auth import model as pam
 from ..contest import model as pcm
 from ..controller import db
 
@@ -35,7 +36,7 @@ class Nominee(db.Model, common.ModelWithDates):
     Fields:
         biv_id: primary ID
         display_name: nominated project name
-        website: nominated website
+        url: nominated website
         is_public: is the project to be shown on the public contestant list?
         is_under_review: enables review of a non-public submission
     """
@@ -44,24 +45,32 @@ class Nominee(db.Model, common.ModelWithDates):
         db.Sequence('nominee_s', start=1011, increment=1000),
         primary_key=True
     )
-    #TODO(mda): determine if a display_name is necessary, then add it if so
+    display_name = db.Column(db.String(100), nullable=False)
     url = db.Column(db.String(100), nullable=False)
     is_public = db.Column(db.Boolean, nullable=False)
     is_under_review = db.Column(db.Boolean, nullable=False)
 
-class Nomination(db.Model, common.ModelWithDates):
-    """database model that carries the information of a website nomination
+    def get_contest(self):
+        """Returns the Contest model which owns this Contestant"""
+        return NUContest.query.select_from(pam.BivAccess).filter(
+            pam.BivAccess.source_biv_id == NUContest.biv_id,
+            pam.BivAccess.target_biv_id == self.biv_id
+        ).one()
+    
+
+class Nominator(db.Model, common.ModelWithDates):
+    """database model that carries the information of a website nominator
 
     Fields:
         biv_id: primary ID
         nominee: Foreign key to a Nominee
+        display_name: nominator's name
         client_ip: client ip of the user who performed the nomination
-        submission_datetime: date and time of the nomination
         browser_string: user's browser string at time of submission
     """
     biv_id = db.Column(
         db.Numeric(18),
-        db.Sequence('nomination_s', start=1012, increment=1000),
+        db.Sequence('nominator_s', start=1012, increment=1000),
         primary_key=True
     )
     nominee = db.Column(
@@ -69,10 +78,11 @@ class Nomination(db.Model, common.ModelWithDates):
         db.ForeignKey('nominee.biv_id'),
         nullable=False
     )
+    display_name = db.Column(db.String(100), nullable=False)
     client_ip = db.Column(db.String(45))
     submission_datetime = db.Column(db.DateTime)
     browser_string = db.Column(db.String(200))
 
 Nominee.BIV_MARKER = biv.register_marker(11, Nominee)
-Nomination.BIV_MARKER = biv.register_marker(12, Nomination)
-Nomination.BIV_MARKER = biv.register_marker(13, NUContest)
+Nominator.BIV_MARKER = biv.register_marker(12, Nominator)
+NUContest.BIV_MARKER = biv.register_marker(13, NUContest)
