a68caa32e4641d81fa15ff274347471e451e27e4
Michael Asnes <mike.asnes@gmail.com>
2015-02-03 14:43:25 -0700

    Revert model update adding separate Nominee_url model
    Undoing changes due to YAGNI principle.
    New solution is to have the server follow the
    given url on Nominee submition, follow any redirects, then
    record the final url post-redirects.
    
    Revert "added get_urls method to Nominee"
    Revert "Replaced Nominee.url with Nominee_url database item"
    
    This reverts commits
    39bf9454f4793bddf79cc784b38637569c4bec95
    5deba3a800e383dbac94eaae89472a1e2014cfb2.

--- publicprize/nextup/model.py
+++ publicprize/nextup/model.py
@@ -54,15 +54,9 @@ class Nominee(db.Model, common.ModelWithDates):
         primary_key=True
     )
     display_name = db.Column(db.String(100), nullable=False)
+    url = db.Column(db.String(100), nullable=False)
     is_public = db.Column(db.Boolean, nullable=False)
     is_under_review = db.Column(db.Boolean, nullable=False)
-
-    def get_urls(self):
-        """Returns all urls linked with this Contestant"""
-        return Nominee_url.query.select_from(pam.BivAccess).filter(
-            pam.BivAccess.source_biv_id == self.biv_id,
-            pam.BivAccess.target_biv_id == Nominee_url.biv_id
-        ).all()
 
     def get_contest(self):
         """Returns the Contest model which owns this Contestant"""
@@ -70,31 +64,6 @@ class Nominee(db.Model, common.ModelWithDates):
             pam.BivAccess.source_biv_id == NUContest.biv_id,
             pam.BivAccess.target_biv_id == self.biv_id
         ).one()
-
-
-class Nominee_url(db.Model, common.ModelWithDates):
-    """ submitted urls for a single nominee
-
-    Fields:
-        biv_id: primary ID
-        Nominee: Foreign Key to a nominee
-        url: nominated website
-
-    ex: bivio.biz
-        - https://www.bivio.biz
-        - http://www.bivio.biz
-    """
-    biv_id = db.Column(
-        db.Numeric(18),
-        db.Sequence('nominee_url_s', start=1014, increment=1000),
-        primary_key=True,
-    )
-    nominee = db.Column(
-        db.Numeric(18),
-        db.ForeignKey('nominee.biv_id'),
-        nullable=False
-    )
-    url = db.Column(db.String(100), nullable=False)
 
 
 class Nominator(db.Model, common.ModelWithDates):
