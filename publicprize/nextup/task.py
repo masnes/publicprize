e2c8f20c2b48854a9c3d0573d0e541ead9f8945c
Paul Moeller <moeller@bivio.biz>
2015-02-12 18:14:07 -0700

    added Nominee.action_nominate() which renders with social metadata

--- publicprize/nextup/task.py
+++ publicprize/nextup/task.py
@@ -5,11 +5,13 @@
     :license: Apache, see LICENSE for more details.
 """
 
+import flask
+
 from . import form as pnf
 from .. import common
 from .. import controller as ppc
 
-_template = common.Template('nextup');
+_template = common.Template('nextup')
 
 class NUContest(ppc.Task):
     """Next Up Contest actions"""
@@ -39,12 +41,31 @@ class Nominee(ppc.Task):
     def action_admin_edit_nominee(biv_obj):
         """Admin edit nominees"""
         return pnf.NomineeEdit(obj=biv_obj).execute(biv_obj)
-    
+
     def action_nominate(biv_obj):
         return _template.render_template(
             biv_obj.get_contest(),
             'nominate-thank-you',
             nominee=biv_obj,
-            nominee_url=biv_obj.format_absolute_uri('nominate'),
+            nominee_url=biv_obj.format_absolute_uri('nominees'),
             nominee_tweet="I just nominated " + biv_obj.display_name
-        );
+        )
+
+    def action_nominees(biv_obj):
+        """Public list of nominated websites, with the current nominee
+        for social media meta links."""
+        return _template.render_template(
+            biv_obj.get_contest(),
+            'nominees',
+            nominee=biv_obj,
+            nominee_url=biv_obj.format_absolute_uri('nominees'),
+            nominee_tweet="I just nominated " + biv_obj.display_name,
+            nextup_icon_url=flask.url_for(
+                'static',
+                _external=True,
+                _scheme=(
+                    'http' if ppc.app().config['PUBLICPRIZE']['TEST_MODE']
+                    else 'https'),
+                filename='img/nextup-200x200.png')
+        )
+
