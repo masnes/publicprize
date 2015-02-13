92203ec88fc7fd3db36423d9c490eb0225195780
Paul Moeller <moeller@bivio.biz>
2015-02-13 22:53:16 +0000

    added logger

--- uwsgi.py
+++ uwsgi.py
@@ -1,5 +1,14 @@
 import publicprize.controller as ppc
+import logging
+import logging.handlers;
 
 # Needs to be explicit
 ppc.init()
 app = ppc.app()
+
+handler = logging.handlers.RotatingFileHandler(
+    '/var/log/publicprize/app.log', maxBytes=10000, backupCount=1)
+handler.setLevel(logging.INFO)
+handler.setFormatter(logging.Formatter(
+        '[%(asctime)s] {%(pathname)s:%(lineno)d} %(levelname)s - %(message)s'))
+app.logger.addHandler(handler)
